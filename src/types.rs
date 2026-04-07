80a197763f0ed456c4b35cfcba3da1714f652445
Iñaki Salcedo <iniaki.salcedo@gmail.com>
2026-04-07 13:24:33 +1000

    Add Branch enum for multi-rev solutions

--- src/types.rs
+++ src/types.rs
@@ -6,3 +6,14 @@ pub enum Direction {
     /// Long-way transfer (pi < theta < 2*pi)
     Retrograde,
 }
+
+/// Which branch of a multi-revolution solution.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Branch {
+    /// The single fractional-orbit solution (N = 0)
+    Fractional,
+    /// Lower branch: faster transfer for the given N (a < a_min_energy for that N)
+    Lower,
+    /// Upper branch: slower transfer for the given N (a > a_min_energy for that N)
+    Upper,
+}
